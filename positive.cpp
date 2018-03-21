0da488a40e73a52efa39bc95297677b69813cb33
Pavithramanohar <37619385+Pavithramanohar@users.noreply.github.com>
2018-03-21 16:31:25 +0530

    Create positive.cpp

---
+++ positive.cpp
@@ -0,0 +1,21 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int a;
+cout<<"enter the value of a:";
+cin>>a;
+if(a>0)
+{
+cout<<"a is positive";
+}
+elseif(a<0)
+{
+cout<<"a is negative";
+}
+else
+{
+cout<<"a is zero";
+}
+getch();
+}
