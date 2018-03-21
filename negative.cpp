080d5a09bf7c4241f36129e7df05ec8c875313d2
Pavithramanohar <37619385+Pavithramanohar@users.noreply.github.com>
2018-03-21 16:34:53 +0530

    Create negative.cpp

---
+++ negative.cpp
@@ -0,0 +1,21 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int b;
+cout<<"enter the value of b:";
+cin>>b;
+if(b>0)
+{
+cout<<"b is positive";
+}
+elseif(b<0)
+{
+cout<<"b is negative";
+}
+else
+{
+cout<<"b is zero";
+}
+getch();
+}
